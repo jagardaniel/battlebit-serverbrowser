c618fe2255cd69e97e974b682507ea3d9147a24b
Daniel Matsson <jagardaniel@gmail.com>
2023-07-24 16:19:24 +0200

    Added the new game mode

--- types/GameModes.ts
+++ types/GameModes.ts
@@ -1,4 +1,6 @@
 export const GameModes = {
+  // The GetServerList shows 18 as game mode instead of CTF right now, probably a bug
+  18: "Capture The Flag",
   CONQ: "Conquest",
   DOMI: "Domination",
   ELI: "Elimination",

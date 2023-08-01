94fbed5344c56fa69e8a5308729f5e84698a9ab7
Daniel Matsson <jagardaniel@gmail.com>
2023-08-02 00:46:10 +0200

    Public API has been fixed, change game mode name for CTF

--- types/GameModes.ts
+++ types/GameModes.ts
@@ -1,6 +1,5 @@
 export const GameModes = {
-  // The GetServerList shows 18 as game mode instead of CTF right now, probably a bug
-  18: "Capture The Flag",
+  CaptureTheFlag: "Capture The Flag",
   CONQ: "Conquest",
   DOMI: "Domination",
   ELI: "Elimination",
